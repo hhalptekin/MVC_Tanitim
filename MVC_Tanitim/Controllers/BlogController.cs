bce96ba32410445d813489c2569382419aeb7772
HHALPTEKİN <alptekin_-_@hotmail.com>
2019-05-28 16:29:03 +0300

    Add project files.

---
+++ MVC_Tanitim/Controllers/BlogController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Tanitim.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Category()
+        {
+            return View();
+
+        }
+
+        public ActionResult Popular()
+        {
+            return View();
+        }
+
+        public ActionResult Tag()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+    }
+}
